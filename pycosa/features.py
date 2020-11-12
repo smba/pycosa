b1fc43c8632e19ecfe4e9e641a3c251ce4062d4f
stefan <stefan@debian>
2020-11-12 21:16:34 +0100

    BitVector-based implementations

---
+++ pycosa/features.py
@@ -0,0 +1,103 @@
+from typing import Sequence
+import logging
+import z3
+import numpy as np
+
+class FeatureModel(object):
+    '''
+    
+    '''
+
+    def __init__(self, name: str, src: str):
+
+        self.name = name
+        clauses, feature_dict = FeatureModel.__parse_dimacs(src)
+
+        self.clauses, self.target = FeatureModel.__convert_dimacs_to_bitvec(clauses, len(feature_dict))
+        self.feature_dict = feature_dict
+        
+    @staticmethod
+    def __parse_dimacs(path: str) -> (Sequence[Sequence[int]], dict):
+        '''
+        :param path:
+        :return:
+        '''
+    
+        dimacs = list()
+        dimacs.append(list())
+        with open(path) as mfile:
+            lines = list(mfile)
+    
+            # parse names of features from DIMACS comments (lines starting with c)
+            feature_lines = list(filter(lambda s: s.startswith("c"), lines))
+    
+            # remove comments
+    
+        dimacs = list()
+        dimacs.append(list())
+        with open(path) as mfile:
+            lines = list(mfile)
+    
+            # parse names of features from DIMACS comments (lines starting with c)
+            feature_lines = list(filter(lambda s: s.startswith("c"), lines))
+            feature_names = dict(map(lambda l: (int(l.split(" ")[1]), l.split(" ")[2].replace("\n", "")), feature_lines))
+    
+            # remove comments
+            lines = list(filter(lambda s: not s.startswith("c"), lines))
+
+            for line in lines:
+                tokens = line.split()
+                if len(tokens) != 0 and tokens[0] not in ("p", "c"):
+                    for tok in tokens:
+                        lit = int(tok)
+                        if lit == 0:
+                            dimacs.append(list())
+                        else:
+                            dimacs[-1].append(lit)
+            assert len(dimacs[-1]) == 0
+            dimacs.pop()
+        
+        return dimacs, feature_names
+    
+    @staticmethod
+    def __convert_dimacs_to_bitvec(dimacs: Sequence[Sequence[int]], n_options: int) -> (Sequence[z3.Or], z3.BitVec):
+        
+        clauses = []
+        target = z3.BitVec('target', n_options)
+
+        # add clauses of variability model
+        for clause in dimacs:
+            c = []
+            for opt in clause:
+                opt_sign = 1 if opt >= 0 else 0
+                optid = n_options - abs(opt)
+                c.append(z3.Extract(optid, optid, target) == opt_sign)
+    
+            clauses.append(z3.Or(c))
+    
+        return clauses, target
+    
+    @staticmethod
+    def config_to_int(config: np.ndarray) -> int:
+        '''
+        :param config:
+        :return:
+        '''
+        pass
+    
+    @staticmethod
+    def int_to_config(i: int, n_options: int) -> np.ndarray:
+        '''
+        :param i:
+        :param n_options:
+        :return:
+        '''
+    
+        
+    
+        offset = n_options - int(np.ceil(np.log2(i)))
+        binary_string = "0" * offset + str(bin(i))[2:]
+        assert(len(binary_string) == n_options)
+    
+        binary = np.array([int(bit) for bit in binary_string])
+        return binary
---
+++ pycosa/sampling.py
@@ -0,0 +1,125 @@
+'''
+Created on Nov 12, 2020
+
+@author: stefan
+'''
+import z3
+import numpy as np
+import pandas as pd
+
+from pycosa.features import FeatureModel
+import logging
+import warnings
+import itertools
+
+class Sampler:
+    
+    def __init__(self, fm):
+        self.fm = fm
+
+class CoverageSampler:
+    '''
+    classdocs
+    '''
+    
+    def __init__(self, fm):
+        '''
+        Constructor
+        '''
+        self.fm = fm
+        
+    def sample(self, t: int):
+        n_options = len(self.fm.feature_dict)
+  
+        clauses = self.fm.clauses
+        target = self.fm.target
+        
+        clauses = z3.And(clauses)
+    
+        solutions = []
+        for interaction in itertools.combinations(np.arange(n_options), t):
+            optimizer = z3.Optimize()
+            
+            # assertions
+            optimizer.add(clauses)
+            
+            for opt in interaction:
+                opt = opt.item()
+                constraint = z3.Extract(opt, opt, target) == 1
+                optimizer.add(constraint)
+            
+            func = z3.Sum([z3.ZeroExt(n_options, z3.Extract(i, i, target)) for i in range(n_options)])
+            optimizer.minimize(func)
+            
+            if optimizer.check() == z3.sat:
+                solution = optimizer.model()[target]
+                optimizer.add(solution != target)
+                solutions.append(solution)
+        
+        solutions = [FeatureModel.int_to_config(solution.as_long(), n_options) for solution in solutions]
+        solutions = np.vstack(solutions) == 1
+        solutions = pd.DataFrame(solutions, columns = list(self.fm.feature_dict.values()))
+    
+        return solutions
+    
+    
+class DistanceSampler:
+    
+    def __init__(self, fm: FeatureModel):
+        self.fm = fm
+    
+    def sample(self, size: int):
+        
+        n_options = len(self.fm.feature_dict)
+        origin = z3.BitVecVal("0" * n_options, n_options)
+  
+        clauses = self.fm.clauses
+        target = self.fm.target
+        
+        clauses = z3.And(clauses)
+        
+        solvers = {i: z3.Solver() for i in range(1, n_options)}
+        for index in solvers.keys(): 
+            solvers[index].add(clauses)
+            solvers[index].add(DistanceSampler.__hamming(origin, target, 1) == index)
+            
+        # set of existing solutions
+        solutions = []
+        
+        # unsatisfiable distances
+        available_distances = list(range(1, n_options))
+        unsatisfiable = False
+        sample_sized_reached = False
+        
+        while not (unsatisfiable or sample_sized_reached):
+            distance = np.random.choice(available_distances)
+
+            if solvers[distance].check() == z3.sat:
+                solution = solvers[distance].model()[target]
+                solvers[distance].add(target != solution)
+                solutions.append(solution)
+            else:
+                available_distances.remove(distance)
+    
+            unsatisfiable = (len(available_distances) == 0)
+            sample_sized_reached = (len(solutions) == size)
+                    
+        solutions = [FeatureModel.int_to_config(solution.as_long(), n_options) for solution in solutions]
+        solutions = np.vstack(solutions) == 1
+        solutions = pd.DataFrame(solutions, columns = list(self.fm.feature_dict.values()))
+    
+        return solutions
+    
+    @staticmethod
+    def __hamming(V1, V2, target):
+        h = V1 ^ V2
+        s = max(target.bit_length(), V1.size().bit_length())
+        return z3.Sum([z3.ZeroExt(s, z3.Extract(i, i, h)) for i in range(V1.size())])
+
+
+a = FeatureModel("fm", "/home/stefan/model.dimacs")
+b = CoverageSampler(a)
+sample = b.sample(1)
+import matplotlib.pyplot as plt
+plt.pcolormesh(sample)
+plt.show()
