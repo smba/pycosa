0f384a319e46aa0675955bd2b2c858a743263821
Stefan <stefan@byom.de>
2021-03-12 16:14:25 +0100

    added

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -1,7 +1,32 @@
 from unittest import TestCase
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
 
-class TestSampler(TestCase):
-    def test_sample_to_dataframe(self):
-        solution = [0,1,1,0]
-        self.fail()
+from pycosa.features import FeatureModel
+from pycosa.sampling import DiversityPromotionSampler, NaiveRandomSampler, DistanceSampler
+
+
+class TestImportanceDistributionSampler(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_compute_distribution(self):
+        fm = FeatureModel(src='feature_models/h2.dimacs')
+        literals = fm.feature_dict
+
+        dimacs = fm.clauses_raw
+        expr = FeatureModel._dimacs_to_str(dimacs, literals)
+
+        overall = FeatureModel._compute_partition(expr, literals.values())
+
+        for part in overall:
+            for term in part:
+                print(term)
+            print('-------')
+
+
+
